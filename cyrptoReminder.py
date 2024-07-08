130da8db231835122936e33ee0b90c1528d09db8
Dominique Desert <160805897+dominiquedes@users.noreply.github.com>
2024-07-07 21:18:56 -0400

    Update cyrptoReminder.py

--- cyrptoReminder.py
+++ cyrptoReminder.py
@@ -5,13 +5,13 @@ from email.mime.text import MIMEText
 import gspread
 from google.oauth2.service_account import Credentials
 
-API_KEY = "CG-wuS5YXeXw7wYSHqDRJs9o4sq"
+API_KEY = "Coin Gecko API KEY"
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-CREDS_FILE = r'C:\Users\domin\python-projects\credentials.json'
+CREDS_FILE = r'path to credentials'
 creds = Credentials.from_service_account_file(CREDS_FILE, scopes=SCOPES)
 client = gspread.authorize(creds)
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1shwnwqT3ukyIAe1dk6sKcbwpI0dMuOArMk4dkgK_4Vk/edit?gid=0#gid=0").sheet1
+sheet = client.open_by_url("yourr spreadsheet url").sheet1
 
 url = f'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&x_cg_demo_api_key={API_KEY}'
 headers = {"accept": "application/json"}
@@ -28,18 +28,18 @@ active_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 
 port = 587
-password = "gmpe dtmp wdme rtqi"
+password = "google password"
 
 text = f'Coin: {all_data['name']}, Price: {all_data['current_price']}, Time Checked: {active_time}'
 message = MIMEText(text, "plain")
 message["Subject"] = "CRYPTO-UPDATE"
-message["From"] = "dominiquedesertb@gmail.com"
-message["To"] = "dominiquedesertb@gmail.com"
+message["From"] = "email"
+message["To"] = "recievers email"
 
 with smtplib.SMTP("smtp.gmail.com", port) as server:
     server.starttls()  # Secure the connection
-    server.login("dominiquedesertb@gmail.com", password)
-    server.sendmail("dominiquedesertb@gmail.com", "dominiquedesertb@gmail.com", message.as_string())
+    server.login("email", password)
+    server.sendmail("email", "recievers email", message.as_string())
 
 def add_data_to_next_row():
     try:
